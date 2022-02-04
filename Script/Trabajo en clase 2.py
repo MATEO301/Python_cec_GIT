67c43b15257304256d670eba6aa8f38a807a6cd7
MATEO301 <98974397+MATEO301@users.noreply.github.com>
2022-02-03 23:46:04 -0500

    Add files via upload

---
+++ Script/Trabajo en clase 2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  3 23:34:12 2022
+
+@author: mateobonilla
+"""
+space=" "
+nombre=input("Hola, Como te llamas?")
+print(nombre)
+apellido=input("Y tu apellido, Cual es?")
+print("Un gusto de conocerte",nombre+space+apellido )
+edad=input("Cuantos años tienes?")
+lugar=input("En donde vives?")
+
+print("Hola, me llamo"+space+nombre+space+apellido+
+      space+"tengo"+space+edad+space+"años"+space+"y vivo en "+space
+      +lugar)
